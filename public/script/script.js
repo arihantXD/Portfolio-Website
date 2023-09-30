85595d020aebe1e45ae5d8856dfb58c3159c5198
arihantXD <kamdearihant01@gmail.com>
2023-09-30 13:18:16 +0530

    project section update complete

--- public/script/script.js
+++ public/script/script.js
@@ -4,49 +4,49 @@ cartTrigger.addEventListener("click", () => {
     cart.classList.toggle("active");
 })
 
-const tl = gsap.timeline();
+// const tl = gsap.timeline();
 
-tl
-.from(".navbar" ,{
-    delay : 0.5,
-    opacity : 0,
-    duration : 0.5
-})
-.from(".logo-section" ,{
-    top: 20,
-    opacity : 0,
-    duration : 0.5
-})
-.from(".logo-section-right" ,{
-    top: 30,
-    opacity : 0,
-    duration : 0.5
-})
-.from(".logo-section-left" ,{
-    top: 30,
-    opacity : 0,
-    duration : 0.5
-})
-.from(".slider ion-icon" ,{
-    opacity : 0,
-    duration : 0.5
-})
-.from(".about-header" ,{
-    y: 30,
-    opacity: 0,
-    duration : 0.5
-})
-.from(".about-child" ,{
-    y: 30,
-    opacity: 0,
-    duration : 0.5
-})
-.to(".slider ion-icon" ,{
-    y: 30,
-    yoyo : "true",
-    repeat : -1,
-    duration : 0.8
-})
+// tl
+// .from(".navbar" ,{
+//     delay : 0.5,
+//     opacity : 0,
+//     duration : 0.5
+// })
+// .from(".logo-section" ,{
+//     top: 20,
+//     opacity : 0,
+//     duration : 0.5
+// })
+// .from(".logo-section-right" ,{
+//     top: 30,
+//     opacity : 0,
+//     duration : 0.5
+// })
+// .from(".logo-section-left" ,{
+//     top: 30,
+//     opacity : 0,
+//     duration : 0.5
+// })
+// .from(".slider ion-icon" ,{
+//     opacity : 0,
+//     duration : 0.5
+// })
+// .from(".about-header" ,{
+//     y: 30,
+//     opacity: 0,
+//     duration : 0.5
+// })
+// .from(".about-child" ,{
+//     y: 30,
+//     opacity: 0,
+//     duration : 0.5
+// })
+// .to(".slider ion-icon" ,{
+//     y: 30,
+//     yoyo : "true",
+//     repeat : -1,
+//     duration : 0.8
+// })
 // .from(".about > .about-child" ,{
 //     scrollTrigger : {
 //         trigger : ".intro-div",
